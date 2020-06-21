200538aba3d4a2aea4cd20de02dfb9e8dbda471a
Ebaa Abdelfadeel <ebaa.abdelfadeel@gmail.com>
2020-06-21 00:14:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, detail_hash|
+    detail_hash.each do |detail, name_array|
+      name_array.each do |bird|
+        if pigeon_list[bird] == nil
+          pigeon_list[bird] = {}
+        end
+        if pigeon_list[bird][attribute] == nil
+          pigeon_list[bird][attribute] = []
+        end
+         pigeon_list[bird][attribute].push(detail.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
+
